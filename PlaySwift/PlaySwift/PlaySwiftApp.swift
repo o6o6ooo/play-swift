4b6bbcb2fad810f7edd94a98e1005960d904bfe3
Sakura Wallace <066sakura@gmail.com>
2026-06-22 01:50:53 +0100

    feat: Initial Play Swift project

---
+++ PlaySwift/PlaySwift/PlaySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySwiftApp.swift
+//  PlaySwift
+//
+//  Created by Sakura Wallace on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlaySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaySwift/PlaySwiftTests/PlaySwiftTests.swift
@@ -0,0 +1,19 @@
+//
+//  PlaySwiftTests.swift
+//  PlaySwiftTests
+//
+//  Created by Sakura Wallace on 22/06/2026.
+//
+
+import Testing
+@testable import PlaySwift
+
+struct PlaySwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
