2a941d421794c82ce9fdb9bf4a0b1d58ebdc2b7f
Sakura Wallace <066sakura@gmail.com>
2026-06-30 16:09:44 +0100

    feat: create custom tab bar app

---
+++ swift-lab/CustomTabBar/CustomTabBar/CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Sakura Wallace on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
