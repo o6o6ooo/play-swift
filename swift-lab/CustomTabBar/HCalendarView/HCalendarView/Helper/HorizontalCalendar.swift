2f1664ad7a40057b609dd0d592c302cf6b422b95
Sakura Wallace <066sakura@gmail.com>
2026-07-08 15:55:07 +0100

    feat: horizontal calendar view following the youtube video

---
+++ swift-lab/CustomTabBar/HCalendarView/HCalendarView/Helper/HorizontalCalendar.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalCalendar.swift
+//  HCalendarView
+//
+//  Created by Sakura Wallace on 08/07/2026.
+//
+
+import SwiftUI
+
+struct HorizontalCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalCalendar()
+}
