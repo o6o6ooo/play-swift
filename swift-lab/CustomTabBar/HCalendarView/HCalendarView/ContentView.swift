b22fd88bb24c88acf5d90e5bb0c499845cec2672
Sakura Wallace <066sakura@gmail.com>
2026-07-08 11:04:12 +0100

    feat: create horizontal calendar view app

---
+++ swift-lab/CustomTabBar/HCalendarView/HCalendarView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HCalendarView
+//
+//  Created by Sakura Wallace on 08/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-lab/CustomTabBar/HCalendarView/HCalendarView/HCalendarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HCalendarViewApp.swift
+//  HCalendarView
+//
+//  Created by Sakura Wallace on 08/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct HCalendarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
