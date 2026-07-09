9b4e9cea28f3d246360c527c6fa33c6d78d63a0b
Sakura Wallace <066sakura@gmail.com>
2026-07-09 10:18:45 +0100

    feat: create glass toast spp

---
+++ swift-lab/GlassToasts/GlassToasts/GlassToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassToastsApp.swift
+//  GlassToasts
+//
+//  Created by Sakura Wallace on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct GlassToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
