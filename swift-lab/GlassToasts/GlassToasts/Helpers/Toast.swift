ad53857292cadf7d64c4bc0dfd7e0bf89541e654
Sakura Wallace <066sakura@gmail.com>
2026-07-09 18:31:16 +0100

    feat: glass toast following the youtube video

---
+++ swift-lab/GlassToasts/GlassToasts/Helpers/Toast.swift
@@ -0,0 +1,108 @@
+//
+//  Toast.swift
+//  GlassToasts
+//
+//  Created by Sakura Wallace on 09/07/2026.
+//
+
+import SwiftUI
+
+struct ToastRootView<Content: View>: View {
+	@ViewBuilder var content: Content
+	@State private var activeToast: Toast?
+	var body: some View {
+		content
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.overlay(alignment: .bottom) {
+				GlassEffectContainer(spacing: 10) {
+					if let activeToast {
+						ToastView(activeToast)
+					}
+				}
+				.opacity(activeToast == nil ? 0 : 1)
+			}
+			.environment(\.showToast) { toast in
+				guard activeToast != nil else {
+					withAnimation(animation) {
+						activeToast = toast
+					}
+					return
+				}
+
+				withAnimation(
+					animation.logicallyComplete(after: 0.17),
+					completionCriteria: .logicallyComplete
+				) {
+					activeToast = nil
+				} completion: {
+					withAnimation(animation) {
+						activeToast = toast
+					}
+				}
+			}
+	}
+	@ViewBuilder
+	private func ToastView(_ toast: Toast) -> some View {
+		HStack(spacing: 10) {
+			if let symbol = toast.symbol {
+				Image(systemName: symbol)
+					.font(.title3)
+					.foregroundStyle(Color.primary)
+					.transition(.identity)
+			}
+			Text(toast.title)
+				.font(.body)
+				.lineLimit(1)
+			Spacer(minLength: 0)
+
+			if let actionTitle = toast.actionTitle, let action = toast.action {
+				Button {
+					if action() {
+						dismiss()
+					}
+				} label: {
+					Text(actionTitle)
+						.foregroundStyle(toast.actionTint)
+				}
+				.transition(.identity)
+			}
+		}
+		.padding(.horizontal, 18)
+		.frame(height: 50)
+		.clipShape(.capsule)
+		.contentShape(.capsule)
+		.glassEffect(.regular, in: .capsule)
+		.padding(.horizontal, 15)
+		.transition(.offset(y: toast.transitionOffset))
+	}
+	private func dismiss() {
+		withAnimation(animation) {
+			activeToast = nil
+		}
+	}
+	private let animation: Animation = .interpolatingSpring(
+		duration: 0.35,
+		bounce: 0.1,
+		initialVelocity: 0
+	)
+}
+
+struct Toast: Identifiable {
+	private(set) var id: String = UUID().uuidString
+	var title: String
+	var duration: CGFloat
+	var placementOffset: CGFloat
+	var transitionOffset: CGFloat = 100
+	var symbol: String? = nil
+	var actionTitle: String? = nil
+	var actionTint: Color = .accentColor
+	var action: (() -> Bool)? = nil
+}
+
+extension EnvironmentValues {
+	@Entry var showToast: (Toast) -> Void = { _ in }
+}
+
+#Preview {
+	ContentView()
+}
